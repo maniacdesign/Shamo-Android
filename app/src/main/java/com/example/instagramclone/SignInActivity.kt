24d9b6b55fe3562bba6db3888b763e349338d9ba
maniacdesign <sigittp@gmail.com>
2021-09-27 14:26:42 +0700

    adding main menu

--- app/src/main/java/com/example/instagramclone/SignInActivity.kt
+++ app/src/main/java/com/example/instagramclone/SignInActivity.kt
@@ -1,6 +1,9 @@
 package com.example.instagramclone
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class SignInActivity : AppCompatActivity() {
@@ -8,5 +11,19 @@ class SignInActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
+        var et_email = findViewById(R.id.editTextEmail) as EditText
+        var et_password = findViewById(R.id.editTextPassword) as EditText
+        var btn_signin = findViewById(R.id.buttonSignIn) as Button
+        var btn_signup = findViewById(R.id.textAccount) as TextView
+
+        btn_signup.setOnClickListener() {
+            setContentView(R.layout.activity_sign_up)
+        }
+
+        btn_signin.setOnClickListener() {
+            setContentView(R.layout.activity_main)
+        }
     }
+
+
 }
