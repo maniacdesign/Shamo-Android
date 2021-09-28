a1aaa37c53cda26b2bdb723ce3ecb8103ac67f53
SIGIT <sigittp@gmail.com>
2021-09-28 22:22:35 +0700

    Creating Bottom Nav

--- app/src/main/java/com/online/shamo/MainActivity.kt
+++ app/src/main/java/com/online/shamo/MainActivity.kt
@@ -2,10 +2,18 @@ package com.online.shamo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val BottomNavigationItemView = findViewById<BottomNavigationItemView>(R.id.bottomNav)
+        val navController = findNavController(R.id.homeFragment)
+
+        BottomNavigationItemView
     }
 }
